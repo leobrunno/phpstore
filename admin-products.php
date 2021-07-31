825f6a776f8f3c72780c2fe8b05fae4b83dd8408
leobrunno <leobrunno@live.com>
2021-07-30 23:36:58 -0300

    Implementando o cadastro de produtos

--- admin-products.php
+++ admin-products.php
@@ -14,3 +14,24 @@ $app->get("/admin/products", function (){
         "products" => $products
     ));
 });
+
+$app->get("/admin/products/create", function (){
+    User::verifyLogin();
+
+    $page = new Hcode\PageAdmin();
+
+    $page->setTpl("products-create");
+});
+
+$app->post("/admin/products/create", function (){
+    User::verifyLogin();
+
+    $product = new Product();
+
+    $product->setData($_POST);
+
+    $product->save();
+
+    header("Location: /phpstore/admin/products");
+    exit();
+});
