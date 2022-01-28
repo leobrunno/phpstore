03c6cfc0d69ee3ec91a4980bd9e8df0ccbadb28e
leobrunno <leobrunno@live.com>
2022-01-27 23:57:44 -0300

    Gerando boletos a partir do projeto boletophp

--- site.php
+++ site.php
@@ -221,7 +221,23 @@ $app->post("/checkout", function(){
 
     $address->save();
 
-    header("Location: /phpstore/order");
+    $cart = Hcode\Model\Cart::getFromSession();
+
+    $cart->getCalculateTotal();
+
+    $order = new \Hcode\Model\Order();
+
+    $order->setData(array(
+        "idcart" => $cart->getidcart(),
+        "idaddress" => $address->getidaddress(),
+        "iduser" => $user->getiduser(),
+        "idstatus" => Hcode\Model\OrderStatus::EM_ABERTO,
+        "vltotal" => $cart->getvltotal()
+    ));
+
+    $order->save();
+
+    header("Location: /phpstore/order/".$order->getidorder());
     exit();
 });
 
@@ -420,4 +436,88 @@ $app->post("/profile", function(){
 
     header("Location: /phpstore/profile");
     exit();
+});
+
+$app->get("/order/:idorder", function($id_order){
+
+    Hcode\Model\User::verifyLogin(false);
+
+    $order = new Hcode\Model\Order();
+
+    $order->get((int)$id_order);
+
+    $page = new Hcode\Page();
+
+    $page->setTpl("payment", array(
+        "order" => $order->getValues()
+    ));
+});
+
+$app->get("/boleto/:idorder", function($id_order){
+
+    Hcode\Model\User::verifyLogin(false);
+
+    $order = new Hcode\Model\Order();
+
+    $order->get((int)$id_order);
+
+    // DADOS DO BOLETO PARA O SEU CLIENTE
+    $dias_de_prazo_para_pagamento = 10;
+    $taxa_boleto = 5.00;
+    $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
+    $valor_cobrado = (float) formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+    $valor_cobrado = str_replace(",", ".",$valor_cobrado);
+    $valor_boleto = number_format($valor_cobrado + $taxa_boleto, 2, ',', '');
+
+    $dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
+    $dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
+    $dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+    $dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+    $dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+    $dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+
+    // DADOS DO SEU CLIENTE
+    $dadosboleto["sacado"] = $order->getdesperson();
+    $dadosboleto["endereco1"] = $order->getdesaddress() . " " . $order->getdesdistrict();
+    $dadosboleto["endereco2"] = $order->getdescity() . " " . $order->getdesstate() . " " . $order->getdescountry() . " - CEP: " . $order->getdeszipcode();
+
+    // INFORMACOES PARA O CLIENTE
+    $dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja PHP STORE";
+    $dadosboleto["demonstrativo2"] = "Taxa bancária - R$ 0,00";
+    $dadosboleto["demonstrativo3"] = "";
+    $dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+    $dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
+    $dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: suporte@phpstore.com.br";
+    $dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto Loja PHP STORE - www.phpstore.com.br";
+
+    // DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+    $dadosboleto["quantidade"] = "";
+    $dadosboleto["valor_unitario"] = "";
+    $dadosboleto["aceite"] = "";		
+    $dadosboleto["especie"] = "R$";
+    $dadosboleto["especie_doc"] = "";
+
+
+    // ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+
+
+    // DADOS DA SUA CONTA - ITAÚ
+    $dadosboleto["agencia"] = "1690"; // Num da agencia, sem digito
+    $dadosboleto["conta"] = "48781";	// Num da conta, sem digito
+    $dadosboleto["conta_dv"] = "2"; 	// Digito do Num da conta
+
+    // DADOS PERSONALIZADOS - ITAÚ
+    $dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+
+    // SEUS DADOS
+    $dadosboleto["identificacao"] = "PHP STORE";
+    $dadosboleto["cpf_cnpj"] = "00.000.000/0001-91";
+    $dadosboleto["endereco"] = "Rua Rasmus Lerdorf, 777 - Qeqertarsuaq, 00000-001";
+    $dadosboleto["cidade_uf"] = "Qeqertarsuaq - Disko";
+    $dadosboleto["cedente"] = "PHP STORE LTDA";
+
+    // NÃO ALTERAR!
+    $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "phpstore" . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR;
+    require_once($path . "funcoes_itau.php");
+    require_once($path . "layout_itau.php");
 });
