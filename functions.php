59666e43b8ad25ba175ba552f9bccbe264f01bc4
leobrunno <leobrunno@live.com>
2022-01-30 22:27:08 -0300

    Exibição do resumo do carrinho no header, com valor total dos produtos e quantidade de items

--- functions.php
+++ functions.php
@@ -18,3 +18,21 @@ function getUserName()
 
     return $user->getdesperson();
 }
+
+function getCartNrQtd()
+{
+    $cart = Hcode\Model\Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return $totals['nrqtd'];
+}
+
+function getCartVlSubTotal()
+{
+    $cart = Hcode\Model\Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return formatPrice($totals['vlprice']);
+}
