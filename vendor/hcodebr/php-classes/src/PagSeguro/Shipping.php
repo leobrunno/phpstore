b520f5de98054f82f01e010e9f66b44d2d2ecd64
leobrunno <leobrunno@live.com>
2022-02-24 06:17:11 -0300

    Alteração na classe Shipping para retorno do objeto xml

--- vendor/hcodebr/php-classes/src/PagSeguro/Shipping.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Shipping.php
@@ -2,6 +2,10 @@
 
     namespace Hcode\PagSeguro;
 
+    use DOMDocument;
+    use DOMElement;
+    use Exception;
+
     class Shipping
     {
         const PAC = 1;
@@ -13,4 +17,40 @@
         private $cost;
         private $addressRequired;
         
+        public function __construct(Address $address, int $type, float $cost, bool $addressRequired = true)
+        {
+            
+            if($type < 1 || $type > 3){
+
+                throw new Exception("Informe um tipo de frete válido!");
+            }
+
+            $this->address = $address;
+            $this->type = $type;
+            $this->cost = $cost;
+            $this->addressRequired = $addressRequired;
+        }
+
+        public function getDOMElement():DOMElement
+        {
+            $dom = new DOMDocument();
+
+            $shipping = $dom->createElement("shipping");
+            $shipping = $dom->appendChild($shipping);
+
+            $address = $this->address->getDOMElement();
+            $address = $dom->importNode($address, true);
+            $address = $shipping->appendChild($address);
+
+            $cost = $dom->createElement("cost", number_format($this->cost, 2, ".", ""));
+            $cost = $shipping->appendChild($cost);
+
+            $type = $dom->createElement("type", $this->type);
+            $type = $shipping->appendChild($type);
+
+            $addressRequired = $dom->createElement("addressRequired", ($this->addressRequired) ? "true" : "false");
+            $addressRequired = $shipping->appendChild($addressRequired);
+            
+            return $shipping;
+        }
     }
