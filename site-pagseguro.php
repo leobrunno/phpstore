6191e841508d57ef5db4c1ef3f956267d5d54394
leobrunno <leobrunno@live.com>
2022-02-25 07:00:45 -0300

    Inicio da montagem do xml a partir da junção das classes necessarias

--- site-pagseguro.php
+++ site-pagseguro.php
@@ -4,6 +4,13 @@ use \Hcode\PagSeguro\Config;
 use \Hcode\Page;
 use \Hcode\Model\User;
 use \Hcode\Model\Order;
+use Hcode\PagSeguro\Address;
+use Hcode\PagSeguro\CreditCard\Holder;
+use Hcode\PagSeguro\CreditCard\Installment;
+use Hcode\PagSeguro\Document;
+use Hcode\PagSeguro\Phone;
+use Hcode\PagSeguro\Sender;
+use Hcode\PagSeguro\Shipping;
 use \Hcode\PagSeguro\Transporter;
 
 $app->post("/payment/credit", function(){
@@ -14,9 +21,43 @@ $app->post("/payment/credit", function(){
 
     $order->getFromSession();
 
+    $order->get((int) $order->getidorder());
+
     $address = $order->getAdress();
 
     $cart = $order->getCart();
+
+    $cpf = new Document(Document::CPF, $_POST['cpf']);
+
+    $phone = new Phone($_POST['ddd'], $_POST['phone']);
+
+    $address = new Address(
+        $address->getdesaddress(),
+        $address->getdesnumber(),
+        $address->getdescomplement(),
+        $address->getdesdistrict(),
+        $address->getdeszipcode(),
+        $address->getdescity(),
+        $address->getdesstate(),
+        $address->getdescountry()
+    );
+
+    $birthDate = new DateTime($_POST['birth']);
+    
+    $sender = new Sender(
+        $order->getdesperson(), 
+        $cpf,
+        $birthDate, 
+        $phone, 
+        $order->getdesemail(), 
+        $_POST['hash']
+    );
+
+    $holder = new Holder($order->getdesperson(), $cpf, $birthDate, $phone);
+
+    $shipping = new Shipping($address, (float) $cart->getvlfreight(), Shipping::PAC);
+
+    $installment = new Installment((int) $_POST['installments_qtd'], (float) $_POST['installments_value']);
 });
 
 $app->get("/payment", function(){
