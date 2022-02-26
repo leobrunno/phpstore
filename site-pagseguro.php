2bdca749b84bee6c9857cb76a20f83532a9bc7dc
leobrunno <leobrunno@live.com>
2022-02-26 05:03:49 -0300

    Configurando classe Payment para adicionar os items do carrinho e o método de pagamento

--- site-pagseguro.php
+++ site-pagseguro.php
@@ -5,9 +5,12 @@ use \Hcode\Page;
 use \Hcode\Model\User;
 use \Hcode\Model\Order;
 use Hcode\PagSeguro\Address;
+use Hcode\PagSeguro\CreditCard;
 use Hcode\PagSeguro\CreditCard\Holder;
 use Hcode\PagSeguro\CreditCard\Installment;
 use Hcode\PagSeguro\Document;
+use Hcode\PagSeguro\Item;
+use Hcode\PagSeguro\Payment;
 use Hcode\PagSeguro\Phone;
 use Hcode\PagSeguro\Sender;
 use Hcode\PagSeguro\Shipping;
@@ -31,7 +34,7 @@ $app->post("/payment/credit", function(){
 
     $phone = new Phone($_POST['ddd'], $_POST['phone']);
 
-    $address = new Address(
+    $shippingAddress = new Address(
         $address->getdesaddress(),
         $address->getdesnumber(),
         $address->getdescomplement(),
@@ -55,9 +58,38 @@ $app->post("/payment/credit", function(){
 
     $holder = new Holder($order->getdesperson(), $cpf, $birthDate, $phone);
 
-    $shipping = new Shipping($address, (float) $cart->getvlfreight(), Shipping::PAC);
+    $shipping = new Shipping($shippingAddress, (float) $cart->getvlfreight(), Shipping::PAC);
 
     $installment = new Installment((int) $_POST['installments_qtd'], (float) $_POST['installments_value']);
+
+    $billingAddress = new Address(
+        $address->getdesaddress(),
+        $address->getdesnumber(),
+        $address->getdescomplement(),
+        $address->getdesdistrict(),
+        $address->getdeszipcode(),
+        $address->getdescity(),
+        $address->getdesstate(),
+        $address->getdescountry()
+    );
+
+    $creditCard = new CreditCard($_POST['token'], $installment, $holder, $billingAddress);
+
+    $payment = new Payment($order->getidorder(), $sender, $shipping);
+
+    foreach ($cart->getProducts() as $product) {
+        
+        $item = new Item(
+            (int) $product["idproduct"],
+            $product["desproduct"],
+            (float) $product["vlprice"],
+            (int) $product["nrqtd"]
+        );
+
+        $payment->addItem($item);
+    }
+
+    $payment->setCreditCard($creditCard);
 });
 
 $app->get("/payment", function(){
